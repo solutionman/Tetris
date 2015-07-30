5b830515e0a3a085bc815f6ad95f244f49bd9152
lom <diskedge@yandex.ru>
2015-07-30 08:56:21 +0700

    First Commit

---
+++ src/Tetris.java
@@ -0,0 +1,55 @@
+
+/**
+ * Created by Dima on 30.06.2015.
+ */
+
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Tetris extends JFrame {
+    JMenuBar menubar;
+    JMenu game;
+    JMenuItem newgame;
+    JMenuItem exit;
+
+    public Tetris(){
+        setLayout(new FlowLayout());
+
+        menubar = new JMenuBar();
+        setJMenuBar(menubar);
+
+        game = new JMenu("Game");
+        menubar.add(game);
+
+        newgame = new JMenuItem("New Game");
+        game.add(newgame);
+
+        exit = new JMenuItem("Exit");
+        game.add(exit);
+
+        event e = new event();
+        exit.addActionListener(e);
+
+    }
+
+    public class event implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+            System.exit(0);
+        }
+    }
+
+    public static void main(String args[]){
+
+        ImageIcon tetrisicon = new ImageIcon("tetris.jpg");
+
+        Tetris gui = new Tetris();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+        gui.setSize(400,600);
+        gui.setTitle("Tetris");
+        gui.setResizable(false);
+        gui.setIconImage(tetrisicon.getImage());
+
+    }
+}
